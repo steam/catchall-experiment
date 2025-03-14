ed0502c0deeb4bba9353a7a418b837f894e00194
Sean Dougherty <seancdougherty@gmail.com>
2025-03-14 17:34:33 -0600

    add catchall api route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ nextauth: string }> }
+) {
+  const { nextauth } = await params;
+  console.log("nextauth", nextauth);
+  return NextResponse.json({
+    success: true,
+  });
+}
